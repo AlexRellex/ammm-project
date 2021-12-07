bcc48aa9000bc8fec53ac0e6cf28a6118f5396ff
Alejandro Alonso Marin <alexander.alonso.marin@gmail.com>
2021-12-07 13:30:14 +0100

    clean the code a bit

--- solverGreedy.py
+++ solverGreedy.py
@@ -1,51 +1,9 @@
-import random, time, pprint
-from datParser import *
-from collections import defaultdict
+from graphManager import *
 
 
-class Solver_Greedy():
+class Solver_Greedy(GraphManager):
     def __init__(self, datAttr):
-        self.datAttr = datAttr
-        self.pretty = pprint.PrettyPrinter()  # for pretty printing
-        self._graphG = defaultdict(set)
-        self._graphH = defaultdict(set)
-        self._create_graph(datAttr.G, self._graphG)   
-        self._create_graph(datAttr.H, self._graphH)      
-        # Initialize the solution
-        self.edges = []
-        self.used_edges = []
-        self.used_diffs = []
-        self.solution = defaultdict(set)       
-    
-    def _create_graph(self, connections, graph):
-        """ Add connections (adjacence matrix) to graph """
-        for i in range(len(connections)):
-            for j in range(len(connections[i])):
-                if connections[i][j] > 0:
-                    self._add(i, j, graph)
-        print("Graph created: ")
-        self.pretty.pprint(graph)
-        #print(f'graph created: {graph}')
-
-    @staticmethod
-    def _add(node1, node2, graph):
-        """ Add connection between node1 and node2 """
-        graph[node1].add(node2)
-        graph[node2].add(node1)
-
-    @staticmethod
-    def _remove_edge(node1, node2, graph):
-        """ Remove all references to node """
-        graph[node1].remove(node2)
-        graph[node2].remove(node1)
-        for node in list(graph):
-            if len(graph[node]) == 0:
-                del graph[node]
-
-    def _is_connected(self, node1, node2, graph):
-        """ Is node1 directly connected to node2 """
-
-        return node1 in graph and node2 in graph[node1]        
+        super().__init__(datAttr)        
 
     def _find_best_candidate(self):
         """ Find the best candidate to add to the solution """
@@ -228,7 +186,7 @@ class Solver_Greedy():
 
 if __name__ == "__main__":
     parser = DATParser()
-    datAttr = parser.decode("project.3.dat")
+    datAttr = parser.decode("project.9.dat")
     solver = Solver_Greedy(datAttr)
     residual = solver.solve()
     if residual is None:
