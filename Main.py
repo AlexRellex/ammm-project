eeb6c852c35eb0f23ecb400c032c2a75306a6629
Alejandro Alonso Marin <alexander.alonso.marin@gmail.com>
2021-12-08 16:45:19 +0100

    run every solver

--- Main.py
+++ Main.py
@@ -1,32 +1,45 @@
-import sys
+from re import A
+import sys, os
 from datParser import DATParser
 from ValidateConfig import ValidateConfig
 from InstanceGenerator import InstanceGenerator
 from AMMMGlobals import AMMMException
+from solverGreedy import Solver_Greedy
+from localSearch import SolverLocalSearch
+from solverGrasp import SolverGrasp
 
 
 def run():
     try:
         configFile = "configfiles/config.dat"
-        print("AMMM Instance Generator")
-        print("-----------------------")
-        print("Reading Config file %s..." % configFile)
-        config = DATParser.parse(configFile)
-        print("Config file parsed...")
+        print("Reading Config file %s" % configFile)
+        parser = DATParser()
+        config = parser.parse(configFile)
+        print("Config file parsed")
         ValidateConfig.validate(config)
-        print("Creating Instances...")
-        instGen = InstanceGenerator(config)
-        instGen.generate()
-        print("Done")
-        return 0
+        print("Creating Instances")
+        insGen = InstanceGenerator(config)
+        status = insGen.generate()
+        if status:
+            raise AMMMException("Could not generate instances, not feasible instances found")
+
+        K = 4
+        alpha = 0.5
+        for file in os.listdir("datafiles"):
+            if file.endswith(".dat"):
+                datAttr = parser.decode("datafiles/" + file)
+                print("Running Solvers for file %s" % file)
+                print(f"Graph G: {datAttr.G}")
+                print(f"Graph H: {datAttr.H}")
+                greedy = Solver_Greedy(datAttr)
+                local = SolverLocalSearch(datAttr, K)
+                grasp = SolverGrasp(datAttr, alpha)
+                greedy.solve()
+                local.solve()
+                grasp.solve()
+
     except AMMMException as e:
         print("Exception: %s", e)
-        return 1
-    except Exception as e:
-        print("#############################")
-        print("UNEXPECTED EXCEPTION HAPPENED %s", e)
-        print("#############################")
-        print(f"{e}")
         return 1
 
 if __name__ == '__main__':
